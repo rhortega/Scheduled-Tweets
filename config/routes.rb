0f313a61a8c1a59f4aef61247dd79e341dc0fa4f
Rodrigo Hortega <hortega.rodrigo@gmail.com>
2021-04-23 21:07:29 +0100

    Route, controller and firts view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # pega informacoes do HTTP e joga para um controle especifico
-  get "about", to: "about#index"
+  root to: "pages#index"
+  get "about-us", to: "pages#about", as: :about
+  
 end
