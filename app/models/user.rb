dca9918dcd2e93cce006d2ac85fa5cedcb39836f
Rodrigo Hortega <hortega.rodrigo@gmail.com>
2021-04-26 21:32:34 +0100

    login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,5 @@
 class User < ApplicationRecord
-  has_secure_passsword
+    has_secure_password
+
+    validates :email, presence: true, format: {with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be a valid email"}
 end
-
-# email: string
-# password_digest: string
-#
-# password:string virtual
-# password_confirmation:string virtual
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # pega informacoes do HTTP e joga para um controle especifico
   root to: "pages#index"
+ 
   get "about-us", to: "pages#about", as: :about
   
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
 end
